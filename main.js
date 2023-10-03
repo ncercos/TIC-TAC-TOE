f78555bb79bced5a415ee8d5d2c14262c220c509
Nicholas <nicholas.cercos@gmail.com>
2023-10-03 14:48:48 -0400

    added win logic

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
@@ -25,7 +36,7 @@ function render() {
     board.forEach(function(mark, index) {
         squares[index].textContent = mark;
     });
-    messages.textContent = `It's ${turn}'s turn!`;
+    messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 };
 render();
 
@@ -37,6 +48,21 @@ function handleTurn(event) {
     board[idx] = turn;
     // Swap to opponent
     turn = turn === 'X' ? '0' : 'X';
+
+    // Check for win
+    win = getWinner();
+
     // Render changes
     render();
+};
+
+function getWinner() {
+    let winner = null;
+
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) 
+            winner = board[combo[0]];
+    });
+
+    return winner;
 };
