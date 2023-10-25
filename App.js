d9c2d5f475258ac8d3960a2dfc7b7be9b95acd6e
Nicholas <nicholas.cercos@gmail.com>
2023-10-25 19:13:46 -0400

    Redid event logic to work with React components

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
 function App() {
+
+/*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+
+/*----- app's state (variables) -----*/
+const [board, setBoard] = React.useState(["","","","","","","","",""])
+let turn;
+let win;
+let ended;
+
+function handleTurn(event) {
+    if(ended === true)return;
+    let idx = event.target.id
+    let newBoard = [...board];
+    newBoard[idx] = turn;
+    setBoard(newBoard)
+    turn = turn === 'X' ? '0' : 'X';
+    //win = getWinner();
+    //if(win != null) ended = true;
+};
+
     return (
         <div>
             <h1>TIC-REACT-TOE</h1>
             <div className="flex-container flex-column">
-            <div className="flex-container flex-wrap" id="board">
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
+            <div className="flex-container flex-wrap" id="board" onClick={handleTurn}>
+                {board.map((value, idx) => {
+                       return (
+                        <div className="square" key={idx} id={idx}>
+                            {value}
+                        </div>
+                    )
+                })}
             </div>
             <h2>It's X's turn!</h2>
             <button id="reset-button">Reset</button>
